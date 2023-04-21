247bcf1f3e1092b51e8948f478bdc1fbb2a83830
Abdelhamid Bouazi [Opus] <Abdelhamid01bouazi@gmail.com>
2023-04-21 07:16:45 +0000

    To do list using react.js & taiwlindcss

--- toDoList/src/App.js
+++ toDoList/src/App.js
@@ -1,5 +1,6 @@
 import './index.css';
 import { useState } from 'react';
+import { Task } from './Task.js';
 
 function App() {
 	const [todoList, setTodoList] = useState([]);
@@ -12,12 +13,11 @@ function App() {
 				taskName: newTask,
 			};
 			setTodoList([...todoList, task]);
-		} else {
+		} else
 			{
 				alert('please type something');
+        setTodoList([...todoList]);
 			}
-			setTodoList([...todoList]);
-		}
 	};
 	const handleChnage = (event) => {
 		setNewTask(event.target.value);
@@ -42,20 +42,7 @@ function App() {
 
 			<div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-4">
 				{todoList.map((task) => {
-					return (
-						<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-							<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-								<th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-									{task.taskName}
-								</th>
-								<td class="px-6 py-4 text-right">
-									<a onClick={() => deleteTask(task.id)} href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" >
-										Delete
-									</a>
-								</td>
-							</tr>
-						</table>
-					);
+					return <Task deleteTask={deleteTask} taskName={task.taskName} id={task.id} />;
 				})}
 			</div>
 		</div>

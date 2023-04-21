cc6fc50ecca8f6b1879778fa5ef2332e809fb32c
Abdelhamid Bouazi [Opus] <Abdelhamid01bouazi@gmail.com>
2023-04-21 06:56:16 +0000

    To do list using react.js & taiwlindcss

---
+++ toDoList/src/App.js
@@ -0,0 +1,64 @@
+import './index.css';
+import { useState } from 'react';
+
+function App() {
+	const [todoList, setTodoList] = useState([]);
+	const [newTask, setNewTask] = useState('');
+
+	const addTask = () => {
+		if (newTask) {
+			const task = {
+				id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+				taskName: newTask,
+			};
+			setTodoList([...todoList, task]);
+		} else {
+			{
+				alert('please type something');
+			}
+			setTodoList([...todoList]);
+		}
+	};
+	const handleChnage = (event) => {
+		setNewTask(event.target.value);
+	};
+
+	const deleteTask = (id) => {
+		setTodoList(todoList.filter((task) => task.id !== id));
+		console.log(id);
+	};
+	return (
+		<div className="grid justify-items-stretch w-full mt-2">
+			<div className="justify-self-center w-full">
+				<div className="flex items-center">
+					<div className="relative w-1/3">
+						<input onChange={handleChnage} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-full" placeholder="Add Task" required/>
+					</div>
+					<button onClick={addTask} className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+						Add
+					</button>
+				</div>
+			</div>
+
+			<div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-4">
+				{todoList.map((task) => {
+					return (
+						<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+							<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+								<th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+									{task.taskName}
+								</th>
+								<td class="px-6 py-4 text-right">
+									<a onClick={() => deleteTask(task.id)} href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" >
+										Delete
+									</a>
+								</td>
+							</tr>
+						</table>
+					);
+				})}
+			</div>
+		</div>
+	);
+}
+export default App;
